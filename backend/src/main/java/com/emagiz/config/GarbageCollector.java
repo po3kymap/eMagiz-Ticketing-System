32f1552da924e548cb719e8181af5fd1074526e3
Ruslan Morozov <ruslan.neth@gmail.com>
2026-06-25 00:18:29 +0200

    Async GC for tickets done

--- backend/src/main/java/com/emagiz/config/GarbageCollector.java
+++ backend/src/main/java/com/emagiz/config/GarbageCollector.java
@@ -13,7 +13,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 @WebListener
-public class GarbageCollectorListener implements ServletContextListener{
+public class GarbageCollector implements ServletContextListener{
     private final ScheduledExecutorService scheduler =
             Executors.newScheduledThreadPool(1);
 
