bf355c363c3f1b709e51df6f84609b8c9215fe65
Ruslan Morozov <ruslan.neth@gmail.com>
2026-06-26 01:19:58 +0200

    reset passwrod, sending emails rn is from ruslan.neth and possible to send to real gmail accs

--- backend/src/main/java/com/emagiz/config/MailConfig.java
+++ backend/src/main/java/com/emagiz/config/MailConfig.java
@@ -8,11 +8,11 @@ public class MailConfig {
         return (v == null || v.isBlank()) ? defaultValue : v;
     }
 
-    public static String getHost()    { return get("SMTP_HOST",     "smtp.ethereal.email"); }
+    public static String getHost()    { return get("SMTP_HOST",     "smtp.gmail.com"); }
     public static int    getPort()    { return Integer.parseInt(get("SMTP_PORT", "587")); }
-    public static String getUsername(){ return get("SMTP_USERNAME", "daphney77@ethereal.email"); }
-    public static String getPassword(){ return get("SMTP_PASSWORD", "cKbREaQtrubvJAgrJK"); }
-    public static String getFrom()    { return get("SMTP_FROM",     "no-reply@emagiz.local"); }
+    public static String getUsername(){ return get("SMTP_USERNAME", "ruslan.neth@gmail.com"); }
+    public static String getPassword(){ return get("SMTP_PASSWORD", "orzw axwf wkfw prky"); }
+    public static String getFrom()    { return get("SMTP_FROM",     "ruslan.neth@gmail.com"); }
     public static String getFrontendResetUrl(){ return get("FRONTEND_RESET_URL",
             "http://localhost/#/reset-password"); }
 }
